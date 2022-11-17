bf20f406307a70ccf4560cdd4ec042c060478eba
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-17 16:43:09 -0500

    created days

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
 var apik = '54a60b82927ece1254952b9dbcf68376';
+var weather = document.getElementsByClassName('container');
+var weather1 = document.getElementById('day1');
+var weather2 = document.getElementById('day2');
+var weather3 = document.getElementById('day3');
+var weather4 = document.getElementById('day4');
+var weather5 = document.getElementById('day5')
 function getLocation(location)  {
 
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apik}`)
@@ -16,8 +22,16 @@ function getWeather(la,lo) {
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${la}&lon=${lo}&appid=${apik}`)
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-})
+    console.log(data)
+    var current = data.name;
+    var date = moment().format("MM/DD/YYYY");
+    document.querySelector('.date').textContent = date;
+    document.querySelector('.cucity').textContent = current;
+    document.querySelector('.temperature1').textContent = data.main.temp;
+    document.querySelector('.windspeed1').textContent = data.wind.speed;
+    document.querySelector('.humidity1').textContent = data.main.humidity; 
+getWeatherfordays(la,lo,data.dt);
+  })
 }
 
 var query = document.getElementById("search-input");
@@ -36,4 +50,3 @@ getLocation(searchval);
 
 
 
-console.log()
